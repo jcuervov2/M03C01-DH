0e7694f73e9659be7e0284039e999ab91319955d
Juan David Cuervo <juan.cuervo@vendemas.com.pe>
2021-06-09 20:56:35 -0500

    Cambio 3 archivos

--- variables.js
+++ variables.js
@@ -1,4 +1,7 @@
-let perro = yazu
-var gato = gardfield 
-const pato = lucas
+let perro = 'yazu'
+var gato = 'gardfield'
+const pato = 'lucas'
 
+console.log( "la variable perro es " + perro )
+console.log( "la variable gato es " + gato )
+console.log( "la variable perro es " + pato )
